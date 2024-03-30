111901e62d6acd5dc458c3889d3b7ddde866bb87
Tom Pan <tommmpn@gmail.com>
2024-03-29 23:55:29 -0400

    - updated home page display for note cards. (added likes) - sort the pdfFiles based on likes and collects amount before displaying (weights: 0.7, 0.3 respectively) - Added a scenario when user is accessing other people's profile page (added a back button in this case to navigate back to the note the current user was looking at) - Updated the icons

--- app/src/main/java/com/example/notify/ui/home/HomePage.kt
+++ app/src/main/java/com/example/notify/ui/home/HomePage.kt
@@ -3,6 +3,7 @@ package com.example.notify.ui.home
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -33,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.SolidColor
@@ -66,8 +68,7 @@ fun HomePage(navController: NavHostController) {
     pdfFiles.forEach { pdfFile ->
         Log.d("HomePageViewModel", "Retrieved PDF File: ${pdfFile.fileName}")
     }
-    lateinit var auth: FirebaseAuth
-    auth = Firebase.auth
+    val auth: FirebaseAuth = Firebase.auth
     val currentUserId = auth.currentUser?.uid
 
     Box {
@@ -199,8 +200,9 @@ fun Bottom(modifier: Modifier = Modifier, currentUserId: String, navController: 
 @Composable
 fun Center(modifier: Modifier=Modifier, navController: NavHostController, pdfFiles: List<PdfFile>,
            currentUserId: String?) {
-    Box(modifier) {
-        NoteList(pdfFiles, navController, currentUserId)
+    val sortedPdfFiles: List<PdfFile> = pdfFiles.sortedByDescending { it.likes * 0.7 + it.collects * 0.3 }
+    Column(modifier) {
+        NoteList(sortedPdfFiles, navController, currentUserId)
     }
 }
 
@@ -235,10 +237,14 @@ private fun TopSection(navController: NavHostController, currentUserId: String) 
                         .padding(end = 8.dp), // Add some space between the search icon and the user icon
                     tint = uiColor
                 )
-                Box(
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_face_2_24),
+                    contentDescription = "Big Elephant",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .size(40.dp)
-                        .background(color = Color.White, shape = CircleShape)
+                        .clip(CircleShape)
+                        .border(3.dp, Color.Black, CircleShape)
                         .clickable { expanded = !expanded }
                 )
             }
--- app/src/main/java/com/example/notify/ui/home/NoteCard.kt
+++ app/src/main/java/com/example/notify/ui/home/NoteCard.kt
@@ -2,18 +2,25 @@ package com.example.notify.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,7 +42,7 @@ import java.nio.charset.StandardCharsets
 fun NoteList(pdfFiles: List<PdfFile>, navController: NavHostController, currentUserId: String?) {
     LazyVerticalGrid(columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(5.dp)) {
-        itemsIndexed(pdfFiles) {index, pdfFile ->
+        itemsIndexed(pdfFiles) { _, pdfFile ->
             if (currentUserId != null) {
                 NoteCard(
                     id = pdfFile.uid,
@@ -68,8 +75,14 @@ fun NoteCard(
     pushKey: String
 ) {
     val encodedUrl = URLEncoder.encode(downloadUrl, StandardCharsets.UTF_8.toString())
+
+    val truncatedTitle: String = title.substring(0, 10) + "..."
+    var truncatedLikes: String = likes.toString()
+    if (likes > 9999) {
+        truncatedLikes = "9999+"
+    }
     Card(
-        onClick = {navController.navigate(route = "Note/$id/$encodedUrl/$pushKey/$currentUserId")},
+        onClick = {navController.navigate(route = "Note/$id/$encodedUrl/$pushKey/$currentUserId/$title")},
         modifier = modifier,
         elevation= CardDefaults.cardElevation(defaultElevation = 5.dp)
     ) {
@@ -81,14 +94,25 @@ fun NoteCard(
                         .height(80.dp)
                         .fillMaxWidth()
                         .padding(10.dp)
-                        .background(color=Color.White),
+                        .background(color = Color.White),
                         contentAlignment = Alignment.BottomStart)
                     {
                         Column() {
-                            Text(contentDescription, style= TextStyle(color= Color.Black, fontSize=14.sp))
-                            Row() {
-                                Text(title, style= TextStyle(color= Color.Black, fontSize=16.sp))
-
+                            Text(contentDescription, style= TextStyle(color= Color.Black, fontSize=13.sp))
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(truncatedTitle, style=TextStyle(color= Color.Black, fontSize=14.sp))
+                                Spacer(Modifier.weight(1f))
+                                Icon(
+                                    tint = Color.Red,
+                                    imageVector = Icons.Filled.Favorite,
+                                    modifier = Modifier.size(20.dp),
+                                    contentDescription = null
+                                )
+                                Spacer(Modifier.width(2.dp))
+                                Text(truncatedLikes, style=TextStyle(color= Color.Black, fontSize=14.sp))
                             }
                         }
                     }
