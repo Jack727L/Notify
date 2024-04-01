a03678c68fbad3a8f008a325307f383b9af20b39
Kevin Zhang <kevindotzhang@gmail.com>
2024-04-01 00:12:48 -0400

    Added context into HomePageViewModel and fileUploadImpl in order to use PdfToImageConverter function added to extract images from pdf files. As well as added in the Textdetection.kt to extract text from images.

--- app/src/main/java/com/example/notify/Services/UploadService/fileUpload.kt
+++ app/src/main/java/com/example/notify/Services/UploadService/fileUpload.kt
@@ -1,20 +1,22 @@
 package com.example.notify.Services.UploadService
 
+import android.content.Context
 import android.net.Uri
 import androidx.compose.runtime.MutableState
 
 interface FileUpload {
-    fun uploadPdfFileToFirebase(toastGenerated: MutableState<Boolean>,
-                                toastMsg: MutableState<String>,
-                                uploadProgress: MutableState<Float>,
-                                fileName: String?,
-                                pdfFileUri: Uri?,
-                                subject: String,
-                                courseNum: String,
-                                term: String,
-                                year: String,
-                                uid: String,
-                                uuid: String)
+    suspend fun uploadPdfFileToFirebase(toastGenerated: MutableState<Boolean>,
+                                        toastMsg: MutableState<String>,
+                                        uploadProgress: MutableState<Float>,
+                                        fileName: String?,
+                                        pdfFileUri: Uri?,
+                                        context: Context,
+                                        subject: String,
+                                        courseNum: String,
+                                        term: String,
+                                        year: String,
+                                        uid: String,
+                                        uuid: String)
 }
 
 interface PdfFilesRetrievalCallback {
--- app/src/main/java/com/example/notify/ui/upload/UploadViewModel.kt
+++ app/src/main/java/com/example/notify/ui/upload/UploadViewModel.kt
@@ -1,23 +1,26 @@
 package com.example.notify.ui.upload
 
 
+import android.content.Context
 import android.net.Uri
 import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 import com.example.notify.Services.UploadService.FileUpload
 import com.example.notify.Services.UserService.User
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
 class UploadViewModel @Inject constructor (
     private val fileUpload: FileUpload,
-    private val user: User
+    private val user: User,
+    @ApplicationContext private val applicationContext: Context
 ): ViewModel() {
     var pdfFileUri: Uri? = null
     var fileName: String? = null
-    fun UploadPdfFileToFirebase(
+    suspend fun UploadPdfFileToFirebase(
         displayToast: MutableState<Boolean>,
         toastMsg: MutableState<String>,
         uploadProgress: MutableState<Float>,
@@ -29,7 +32,7 @@ class UploadViewModel @Inject constructor (
         val myUuid = UUID.randomUUID()
         val myUuidAsString = myUuid.toString()
         val fileUUID = myUuidAsString + user.getCurrentUserId().orEmpty()
-        fileUpload.uploadPdfFileToFirebase(displayToast, toastMsg, uploadProgress, fileName,pdfFileUri, subject, courseNum,
+        fileUpload.uploadPdfFileToFirebase(displayToast, toastMsg, uploadProgress, fileName,pdfFileUri, applicationContext, subject, courseNum,
             term, year, user.getCurrentUserId().orEmpty(), fileUUID)
     }
 }
